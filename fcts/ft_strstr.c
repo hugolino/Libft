c882a038d90a4052b8d1bbde8028f7dfccf5ae08
System Administrator <root@MacBook-Pro-de-Hugo.local>
2015-10-19 17:37:19 +0200

    debut

--- fcts/ft_strstr.c
+++ fcts/ft_strstr.c
@@ -1,6 +1,15 @@
+
 #include "libft.h"
+
 char	*ft_strstr(const char *s1, const char *s2)
 {
-	(void)s2;
-	return ((char *)s1);
+  size_t i;
+
+  i = 0;
+  if (s2[i] == 0)
+    return (s1);
+  while (s2[i])
+    if (s2[i] != s1[i])
+      i++;
+
 }
