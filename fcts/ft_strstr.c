c1f9680ce381aa51d4f503902e16d0bc8c3362be
Hugo DECAUX <hdecaux@e1r6p18.42.fr>
2015-10-26 13:20:49 +0100

    finish

--- fcts/ft_strstr.c
+++ fcts/ft_strstr.c
@@ -1,15 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdecaux <hdecaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/26 08:26:11 by hdecaux           #+#    #+#             */
+/*   Updated: 2015/10/26 10:15:37 by hdecaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(const char *s1, const char *s2)
+char		*ft_strstr(const char *s1, const char *s2)
 {
-  size_t i;
+	size_t	i;
 
-  i = 0;
-  if (s2[i] == 0)
-    return (s1);
-  while (s2[i])
-    if (s2[i] != s1[i])
-      i++;
-
+	i = 0;
+	if (s2[i] == 0)
+		return ((char*)s1);
+	while (s1[i])
+	{
+		if (ft_strncmp(&s1[i], s2, ft_strlen(s2)) == 0)
+			return ((char*)&s1[i]);
+		else
+			i++;
+	}
+	return (NULL);
 }
