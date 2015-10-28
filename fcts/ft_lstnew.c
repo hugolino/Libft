97bd6fe722d0d6c6311e156dff50778d27ab6882
Hugo Decaux <Hugo@MacBook-Pro-de-Hugo.local>
2015-10-28 17:45:27 +0100

    new lstnew

--- fcts/ft_lstnew.c
+++ fcts/ft_lstnew.c
@@ -16,10 +16,10 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*list;
 
-	list = malloc(sizeof(t_list));
+	list = (t_list*)malloc(sizeof(t_list));
 	if (list == NULL)
 		return NULL;
-	if (content == 0)
+	if (content == NULL)
 	{
 		list->content = NULL;
 		list->content_size = 0;
