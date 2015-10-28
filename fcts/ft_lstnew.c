f4d3a0bba07b2714f3a92f4f9e5f1984b0586793
Hugo DECAUX <hdecaux@e1r6p18.42.fr>
2015-10-28 16:52:00 +0100

    lstnew

--- fcts/ft_lstnew.c
+++ fcts/ft_lstnew.c
@@ -1,7 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdecaux <hdecaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/28 16:23:47 by hdecaux           #+#    #+#             */
+/*   Updated: 2015/10/28 16:51:30 by hdecaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-char		*ft_lstnew(void const *content, size_t content_size)
+
+t_list		*ft_lstnew(void const *content, size_t content_size)
 {
-	(void)content;
-	(void)content_size;
-	return (NULL);
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (list == NULL)
+		return NULL;
+	if (content == 0)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		list->content = (void*)content;
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
 }
