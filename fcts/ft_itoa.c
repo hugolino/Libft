37aeed32264d0e5c99cc637731ec4eb2a880ed99
Hugo DECAUX <hdecaux@e1r6p18.42.fr>
2015-10-28 10:57:26 +0100

    finito

--- fcts/ft_itoa.c
+++ fcts/ft_itoa.c
@@ -1,6 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdecaux <hdecaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/28 08:42:05 by hdecaux           #+#    #+#             */
+/*   Updated: 2015/10/28 10:56:54 by hdecaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+int			ft_count(int n)
+{
+	int		d;
+	int		i;
+
+	i = n;
+	d = 0;
+	while ((i / 10) != 0)
+	{
+		d++;
+		i = i / 10;
+	}
+	return (d);
+}
+
+char		*ft_cases(int n)
+{
+	char	*nb;
+
+	if (n == 0)
+	{
+		nb = ft_strnew(1);
+		nb[0] = 48;
+	}
+	if (n == -2147483648)
+	{
+		nb = ft_strnew(11);
+		nb = "-2147483648";
+	}
+	return (nb);
+}
+
 char		*ft_itoa(int n)
 {
-	(void)n;
-	return (ft_strdup(""));
+	int		d;
+	char	*nb;
+	int		a;
+
+	a = n;
+	d = ft_count(n);
+	if (n == 0 || n == -2147483648)
+		return (ft_cases(n));
+	if (n < 0)
+	{
+		nb = ft_strnew(d + 2);
+		n = -n;
+		d = d + 1;
+	}
+	if (n > 0)
+		nb = ft_strnew(d);
+	while (n != 0)
+	{
+		nb[d] = (n % 10) + 48;
+		n = n / 10;
+		d--;
+	}
+	if (a < 0)
+		nb[0] = '-';
+	return (nb);
 }
